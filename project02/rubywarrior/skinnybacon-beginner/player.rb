7b464e84386ff795bf8b201910864f1a83c56485
Trevor Whitney <trevorjwhitney@gmail.com>
2012-01-17 18:13:39 -0700

    skinnybacon at beginner level 7

--- project02/rubywarrior/skinnybacon-beginner/player.rb
+++ project02/rubywarrior/skinnybacon-beginner/player.rb
@@ -1,21 +1,27 @@
 class Player
   def initialize
     @health = 20
+    @captive_rescued = false
   end
 
   def play_turn(warrior)
     if warrior.feel.captive?
       warrior.rescue!
+    elsif warrior.feel(:backward).captive?
+      warrior.rescue! :backward
+      @captive_rescued = true
+    elsif !@captive_rescued
+      warrior.walk! :backward
     elsif warrior.feel.enemy?
       if warrior.feel.to_s == "Archer"
         warrior.attack!
-      elsif warrior.health > 7
+      elsif warrior.health > 17
         warrior.attack!
       else 
         warrior.walk! :backward
       end
     else
-      if warrior.health > 15
+      if warrior.health > 17
         warrior.walk!
       elsif @health > warrior.health
         warrior.walk!
