14102381aac800f2eb3146572aea8b8d22a232d1
Trevor Whitney <trevorjwhitney@gmail.com>
2012-01-30 14:55:37 -0700

    fixed validation typo uncovered by tests

--- project03/app/models/product.rb
+++ project03/app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
   validates :image_url, :format => {
-    :with => %r{\.(gif|jpeg|png)$}i,
+    :with => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG, or PNG image.'
   }
 end
