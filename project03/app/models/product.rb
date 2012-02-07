4dc942ed989b164a2cbd18979119c74949e37ed4
Trevor Whitney <trevorjwhitney@gmail.com>
2012-02-06 18:55:37 -0700

    finished G2, added who bought atom feed

--- project03/app/models/product.rb
+++ project03/app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ActiveRecord::Base
 
   #associations
   has_many :line_items
+  has_many :orders, :through => :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
 
   #validation
