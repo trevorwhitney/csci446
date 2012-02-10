38adfcc37f41a6d91cc6d7a5f799c3fe8bf782e8
Trevor Whitney <trevorjwhitney@gmail.com>
2012-02-10 14:41:57 -0700

    added random subtitle generator. updated styles. moved font-face declaration to main application.css file

--- project04/app/helpers/application_helper.rb
+++ project04/app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def random_message
+    messages = [
+      "... with a name .com hipsters love!",
+      "... what\'s \"Articl\" in Spanish anyway?",
+      "... my cousin works for Bit.ly",
+      "... not just for the country of Spain",
+      "... my favorite radio staion is Last.fm",
+      "... all your \"Artcl\" are belong to us"
+    ]
+    num = rand(messages.size)
+    messages[num]
+  end
 end
