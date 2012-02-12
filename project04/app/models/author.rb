39b35f4f20a2ebf7a7f8da3735bce015f86b7521
Trevor Whitney <trevorjwhitney@gmail.com>
2012-02-12 12:40:38 -0700

    added and styled author validation to add an error for any name containing Pat

--- project04/app/models/author.rb
+++ project04/app/models/author.rb
@@ -2,4 +2,11 @@ class Author < ActiveRecord::Base
   has_many :articles
 
   validates :name, :presence => true
+  validate :name_cannot_be_pat
+
+  def name_cannot_be_pat
+    if name =~ /[P|p]at/
+      errors.add(:name, 'cannot include "Pat". She broke my heart :(')
+    end
+  end
 end
