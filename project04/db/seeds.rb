a3b7a58033478192be8c36c1ba78b23ebf393bac
Trevor Whitney <trevorjwhitney@gmail.com>
2012-02-12 13:17:29 -0700

    updated seed data

--- project04/db/seeds.rb
+++ project04/db/seeds.rb
@@ -5,10 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Author.transaction do
+  2.times do |i|
+    Author.create :name => "Author #{i}"
+  end
+end
+
 Article.transaction do 
   (1..100).each do |i|
-    Article.create(:title => "Fantastic Article Number #{i}",
+    Article.create(:title => "Fantastic Article #{i}",
       :body => %{Duis ad pitchfork 3 wolf moon odio, voluptate sartorial culpa. Eiusmod cred etsy excepteur occaecat sustainable +1. Consectetur thundercats banh mi gentrify mlkshk. Ullamco banksy brunch terry richardson fap esse officia, master cleanse blog cillum fugiat kogi et small batch. Freegan cillum irure, selvage shoreditch anim fap narwhal id twee aesthetic kale chips pickled. Williamsburg jean shorts high life beard non terry richardson reprehenderit, mcsweeney's master cleanse wayfarers. Small batch odd future seitan tattooed, voluptate incididunt tofu ethnic assumenda bicycle rights pork belly four loko velit.
+        
 Tempor high life fingerstache, raw denim seitan american apparel fixie eiusmod. Sustainable pitchfork keffiyeh hoodie pork belly post-ironic cupidatat deserunt, mumblecore ethical selvage ullamco photo booth bicycle rights godard. Nihil hoodie kale chips, elit photo booth cosby sweater freegan. Vice tofu synth ad, fixie elit delectus farm-to-table sunt authentic. Assumenda lomo post-ironic polaroid 3 wolf moon. Chambray mlkshk eiusmod, portland seitan whatever et quinoa forage. Dolor odd future fugiat locavore.
 
 Tattooed skateboard pork belly, lo-fi banh mi pop-up consectetur biodiesel voluptate vice fingerstache wayfarers keffiyeh. Pork belly farm-to-table typewriter ut fixie, reprehenderit occupy sunt 8-bit adipisicing shoreditch sed semiotics. Magna letterpress etsy selvage aliqua 8-bit, aute sint pitchfork beard trust fund kogi gluten-free vice adipisicing. Exercitation skateboard cred, commodo fugiat mixtape keffiyeh seitan id terry richardson letterpress placeat food truck cardigan cliche. In odio art party proident. Velit pariatur hella seitan nihil. Hoodie est 8-bit sed enim, thundercats exercitation twee williamsburg.
