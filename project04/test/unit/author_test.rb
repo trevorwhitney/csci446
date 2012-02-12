90221ac0062ef2f16136a87b2354468943ea444d
Trevor Whitney <trevorjwhitney@gmail.com>
2012-02-12 12:46:56 -0700

    added name validation tests to author unit tests

--- project04/test/unit/author_test.rb
+++ project04/test/unit/author_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name_should_not_include_pat" do
+    author = Author.new name: "Pat"
+    assert author.invalid?
+
+    author.name = "Patrick"
+    assert author.invalid?
+
+    author.name = "John Patrick"
+    assert author.invalid?
+
+    author.name = "Valid Author"
+    assert author.valid?
+  end
+
+  test "name_should_not_be_blank" do
+    author = Author.new
+    assert author.invalid?
+
+    author.name = "Author Name"
+    assert author.valid?
+  end
 end
