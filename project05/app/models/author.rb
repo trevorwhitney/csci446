92b3f5007e3fcaee0754fecf64f74dbc8d501603
Trevor Whitney <trevorjwhitney@gmail.com>
2012-02-29 00:02:59 -0700

    associated author with picture and updated views

--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,8 +1,15 @@
 class Author < ActiveRecord::Base
   has_many :articles
+  has_attached_file :photo, styles: { small: "150x150>" },
+  					url: "authors/:id/:style/:basename.:extension",
+  					path: ":rails_root/public/assets/authors/:id/:style/:basename.:extension"
 
   validates :name, :presence => true
   validate :name_cannot_be_pat
+
+  validates_attachment_presence :photo
+	validates_attachment_size :photo, :less_than => 5.megabytes
+	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
   def name_cannot_be_pat
     if name =~ /[P|p]at/
