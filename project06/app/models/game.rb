6dcd540f7152b0adba8ddf27e56718f5880535bf
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-04 13:21:35 -0600

    added before_create filter to set user

--- project06/app/models/game.rb
+++ project06/app/models/game.rb
@@ -3,4 +3,13 @@ class Game < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :title
+
+  before_create :set_user
+
+  private
+
+  def set_user
+    self.user = UserSession.find.user
+  end
+
 end
