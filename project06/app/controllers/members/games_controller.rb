e95b486b94e76b42349f05b72b38f222679c069c
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-04 11:51:46 -0600

    added new action and view to Members::GamesController

--- project06/app/controllers/members/games_controller.rb
+++ project06/app/controllers/members/games_controller.rb
@@ -22,13 +22,41 @@ class Members::GamesController < Members::MembersController
 
     if @game.update_attributes(params[:game])
       respond_to do |format|
-        format.html { redirect_to members_games_path, 
-          :success => "Game successfully updated."}
+        format.html do
+          redirect_to members_games_path, 
+           :flash => {:success => "Successfully updated #{@game.title}."}
+        end
       end
     else
       respond_to do |format|
         @form_url = members_game_path
         format.html { render :action => "edit", :template => "games/edit" }
+      end
+    end
+  end
+
+  def new
+    @game = Game.new
+    @form_url = members_games_path
+    render 'games/new'
+  end
+
+  def create
+    @game = Game.new(params[:game])
+
+    if @game.save
+      respond_to do |format|
+        format.html do 
+          redirect_to members_games_path,
+            :flash => {:success => "Sucessfully added #{@game.title}."}
+        end
+      end
+    else
+      respond_to do |format|
+        @form_url = members_games_path
+        format.html do
+          render :action => "new", :template => "games/new"
+        end
       end
     end
   end
