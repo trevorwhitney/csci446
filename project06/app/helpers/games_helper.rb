a2f555fa39d8afd6a2f0ac8e5ccb3537a708f131
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-04 13:21:07 -0600

    added helpers for displaying user specific elements on shared games index

--- project06/app/helpers/games_helper.rb
+++ project06/app/helpers/games_helper.rb
@@ -1,5 +1,12 @@
 module GamesHelper
 
+  def games_nav
+    controller = params[:controller]
+    if controller == "members/games" || controller == :admin
+      raw RedCloth.new("h2. My Gamez").to_html + new_game
+    end
+  end
+
   def game_title(game)
     if current_user.nil?
       game.title
@@ -18,6 +25,16 @@ module GamesHelper
     else
       link_to "Add a game", new_members_game_path
     end
+  end
+
+  def created_at_and_by(game)
+    if game.user == current_user
+      name = "me"
+    else
+      name = game.user.full_name
+    end
+
+    "#{game.created_at.strftime("%b. %-d, %Y")} by #{name}"
   end
 
 end
