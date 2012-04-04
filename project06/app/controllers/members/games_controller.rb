513b3d1428d59b38623892fc5f35e0dd3a17710a
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-03 23:47:54 -0600

    created method and view for edit, as well as game partial

--- project06/app/controllers/members/games_controller.rb
+++ project06/app/controllers/members/games_controller.rb
@@ -11,4 +11,26 @@ class Members::GamesController < Members::MembersController
     render 'games/index'
   end
 
+  def edit
+    @game = Game.find(params[:id])
+    @form_url = members_game_path
+    render 'games/edit'
+  end
+
+  def update
+    @game = Game.find(params[:id])
+
+    if @game.update_attributes(params[:game])
+      respond_to do |format|
+        format.html { redirect_to members_games_path, 
+          :success => "Game successfully updated."}
+      end
+    else
+      respond_to do |format|
+        @form_url = members_game_path
+        format.html { render :action => "edit", :template => "games/edit" }
+      end
+    end
+  end
+
 end
