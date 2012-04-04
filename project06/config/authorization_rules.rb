efcaf00c9a57d39fda224f50be3acd87e3f3a1b0
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-04 13:23:53 -0600

    limited users to only edit their games

--- project06/config/authorization_rules.rb
+++ project06/config/authorization_rules.rb
@@ -1,20 +1,24 @@
 authorization do
   role :guest do
+    has_permission_on :games, :to => :index
   end
 
   role :administrator do
-    has_permission_on [:admin_users, :admin_roles], 
+    has_permission_on [:admin_users, :admin_roles, :admin_games], 
       :to => [:index, :show, :edit, :create, :update, :destroy, :new]
-    has_permission_on [:members_games, :members_users],
+    has_permission_on [:members_games, :members_users, :games],
       :to => :index
     has_permission_on :members_users, :to => :show
   end
 
   role :member do
     has_permission_on :members_games,
-      :to => [:index, :show, :edit, :create, :update, :destroy, :new]
+      :to => [:index, :show, :create, :destroy, :new]
+    has_permission_on :members_games, :to => [:edit, :update] do
+      if attribute :user => is { current_user }
     has_permission_on :members_users,
       :to => [:show, :edit, :update, :index]
+    has_permission_on :games, :to => :index
   end
 
 end
