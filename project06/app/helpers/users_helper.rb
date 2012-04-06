6b66121a4adb0273f8801398b843f07c72da11d4
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-06 14:11:06 -0600

    created and modified helper methods to account for public user methods

--- project06/app/helpers/users_helper.rb
+++ project06/app/helpers/users_helper.rb
@@ -1,15 +1,25 @@
 module UsersHelper
 
   def username_text_field(form)
-    if current_user.is_admin?
+    if current_user.nil?
+      form.text_field :username
+    elsif current_user.is_admin?
       form.text_field :username
     else
       form.text_field :username, :disabled => true
     end
   end
 
+  def user_roles_li(form)
+    unless current_user.nil? || !current_user.is_admin?
+      render 'users/roles_li', :form => form
+    end
+  end
+
   def user_roles_select
-    if current_user.is_admin?
+    if current_user.nil?
+      return
+    elsif current_user.is_admin?
       select("roles", "id", Role.all.map { |r| [r.name, r.id] })
     else
       user_roles
