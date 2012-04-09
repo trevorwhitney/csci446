9a4aed8bdc83e9341e4aba1e4b43f45566d6c73d
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-09 13:56:54 -0600

    default rating_id is now 0

--- project06/db/migrate/20120404042145_create_games.rb
+++ project06/db/migrate/20120404042145_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.string :title
-      t.integer :rating_id
+      t.integer :rating_id, :default => 0
 
       t.timestamps
     end
