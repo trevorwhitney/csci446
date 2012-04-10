c6936dc407acb06b7e389f810eb89c9499771043
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-09 19:32:18 -0600

    added enviornment variable for recaptcha

--- project06/config/environment.rb
+++ project06/config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ENV['RECAPTCHA_PUBLIC_KEY'] = '6LcEBtASAAAAALt_Fk70tCa7sejuu4lwwYMzIaFK'
+ENV['RECAPTCHA_PRIVATE_KEY'] = '6LcEBtASAAAAAB2Wlr7x3afVQTZ-7ItXUdTbnkkJ'
+
 # Initialize the rails application
 Gamez::Application.initialize!
